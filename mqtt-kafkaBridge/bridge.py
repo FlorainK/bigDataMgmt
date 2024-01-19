ff0c06a466d584ad760a5e330ecfecc959ff5542
Florian Kenner <wi21119@lehre.dhbw-stuttgart.de>
2024-01-19 15:10:08 +0100

    Update schema registry URL and Kafka server configuration

--- mqtt-kafkaBridge/bridge.py
+++ mqtt-kafkaBridge/bridge.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from confluent_kafka import Producer
 from confluent_kafka.serialization import StringSerializer, SerializationContext, MessageField
 from confluent_kafka.schema_registry import SchemaRegistryClient
@@ -6,16 +7,17 @@ from confluent_kafka.schema_registry.avro import AvroSerializer
 
 def main():
     topic = "myTopic"
-    schema_registry_url = "http://localhost:8081"
-    kafka_server = "localhost:9094"
+    schema_registry_url = "http://localhost:8083"
+    kafka_server = "localhost:29092"
 
-    schema_registry_client = SchemaRegistryClient({"url": schema_registry_url})
+    schema_regestry_conf = {"url": schema_registry_url}
+    schema_registry_client = SchemaRegistryClient(schema_regestry_conf)
     avro_serializer = AvroSerializer(
-        schema_str='{"type": "record", "name": "myrecord", "fields": [{"name": "f1", "type": "string"}]}',
         schema_registry_client=schema_registry_client,
-        to_dict=True,
+        schema_str='{"type": "record", "name": "myrecord", "fields": [{"name": "f1", "type": "string"}]}',
     )
 
+    string_serializer = StringSerializer("utf_8")
     producer = Producer({"bootstrap.servers": kafka_server})
 
     def delivery_report(err, msg):
@@ -25,14 +27,23 @@ def main():
             print("Message delivered to {} [{}]".format(msg.topic(), msg.partition()))
 
     while True:
-        data = {"f1": "value1"}
-        producer.produce(
-            topic=topic,
-            value=data,
-            on_delivery=delivery_report,
-            value_serializer=lambda v: avro_serializer.encode_record_with_schema(
-                topic=topic, record=v
-            ),
-        )
-        producer.poll(0)
-        producer.flush()
+        producer.poll(0.0)
+        try:
+            data = {"f1": "value1"}
+            producer.produce(
+                topic=topic,
+                on_delivery=delivery_report,
+                key = string_serializer(str(uuid4()), MessageField.KEY),
+                value= avro_serializer(data, SerializationContext(topic, MessageField.VALUE)),
+                ),
+            
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            print(e)
+            continue
+
+    producer.flush()
+
+if __name__ == "__main__":
+    main()
