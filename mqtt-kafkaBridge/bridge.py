923c5c1fe51346dd925e4c161e691d7992272cad
Florian Kenner <wi21119@lehre.dhbw-stuttgart.de>
2024-02-04 14:09:38 +0100

    mvp implementation of windowing

--- mqtt-kafkaBridge/bridge.py
+++ mqtt-kafkaBridge/bridge.py
@@ -1,28 +1,27 @@
-import json, io, os
-
+import json
 import paho.mqtt.client as mqtt
 from kafka import KafkaProducer
-import avro.schema
-from avro.datafile import DataFileWriter
-from avro.io import DatumWriter
+from kafka.admin import KafkaAdminClient, NewTopic
+
 
 
 def main():
     with open("../config.json") as json_file:
         config = json.load(json_file)
 
-    kafka_topic = config["kafka_topic"]
-    kafka_server = config["kafka_server"]
+    kafka_configs = config["kafka_configs"]
     mqtt_broker_address = config["mqtt_broker_address"]
     mqtt_broker_uname = config["mqtt_username"]
 
-    
-    kafka_producer = KafkaProducer(bootstrap_servers=kafka_server)
+    admin_client = KafkaAdminClient(bootstrap_servers=kafka_configs["kafka_server"])
+    if kafka_configs["topic_name"] not in admin_client.list_topics():
+        admin_client.create_topics(new_topics = [NewTopic(name=kafka_configs["topic_name"], num_partitions=kafka_configs["topic_name"], replication_factor=1)])
+    kafka_producer = KafkaProducer(bootstrap_servers=kafka_configs["kafka_server"])
 
 
     def on_message(client, userdata, message):
         print("message received " ,str(message.payload.decode("utf-8")))
-        kafka_producer.send(kafka_topic,value = str(message.payload.decode("utf-8")).encode('utf-8'))
+        kafka_producer.send(kafka_configs["topic_name"],value = str(message.payload.decode("utf-8")).encode('utf-8'))
         kafka_producer.flush()
 
 
--- processing/processing.py
+++ processing/processing.py
@@ -1,26 +1,30 @@
-import io
-import json
-
+import json, datetime
 import faust
-from avro.io import DatumReader, BinaryDecoder
+
 
 def main():
     with open("../config.json") as json_file:
         config = json.load(json_file)
 
-    kafka_server = config["kafka_server"]
-    topic_name = config["kafka_topic"]
+    kafka_configs = config["kafka_configs"]
 
     app = faust.App(
         "processing",
-        broker=kafka_server,
+        broker=kafka_configs["kafka_server"],
+        topic_partitions=8
     )
-    topic = app.topic(topic_name)
+    topic = app.topic(kafka_configs["topic_name"], partitions=kafka_configs["partitions"], internal = True)
+    speed_table = app.Table("speed_table", default = int).tumbling(60)
+
 
     @app.agent(topic)
     async def process(stream):
         async for value in stream:
-            print(value)
+            print(f"Received value: {value}")
+            speed_table['sum'] += value['geschwindigkeit']
+            speed_table['count'] += 1
+
+            print(f"Current sum: {speed_table['sum'].value()}, count: {speed_table['count'].value()}")
     app.main()
 
 if __name__ == "__main__":
